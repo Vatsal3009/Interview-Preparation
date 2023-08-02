94c826ce10b65f0994ffa44fd4a751c652457119
Vatsal3009 <vcdevelopementworld@gmail.com>
2023-08-02 22:04:22 +0530

    Kotlin Version update and gradle update

--- app/src/main/java/com/vcdevelopmentworld/interview/SplashScreen.kt
+++ app/src/main/java/com/vcdevelopmentworld/interview/SplashScreen.kt
@@ -1,48 +1,42 @@
 package com.vcdevelopmentworld.interview
 
-import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.ads.AdView
-import com.google.android.gms.ads.InterstitialAd
-import android.os.Bundle
-import com.vcdevelopmentworld.interview.R
-import com.google.android.gms.ads.MobileAds
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener
-import com.google.android.gms.ads.initialization.InitializationStatus
+import android.animation.ValueAnimator
+import android.annotation.SuppressLint
 import android.content.Intent
-import com.vcdevelopmentworld.interview.AddQuestion
-import com.google.android.gms.ads.AdListener
-import androidx.core.app.NavUtils
-import com.vcdevelopmentworld.interview.MainActivity
-import android.widget.TextView
-import android.os.Build
-import com.vcdevelopmentworld.interview.ResepAdapter.ResepListener
-import androidx.recyclerview.widget.RecyclerView
-import com.vcdevelopmentworld.interview.ResepAdapter
-import androidx.recyclerview.widget.LinearLayoutManager
-import android.content.ActivityNotFoundException
+import android.os.Bundle
 import android.os.Handler
-import hotchemi.android.rate.AppRate
-import hotchemi.android.rate.OnClickButtonListener
-import com.vcdevelopmentworld.interview.ResepAdapter.ResepHolder
-import android.view.ViewGroup
-import android.view.LayoutInflater
 import android.widget.ImageView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import androidx.appcompat.app.AppCompatActivity
+import com.vcdevelopmentworld.interview.MainActivity
+import com.vcdevelopmentworld.interview.databinding.ActivitySplashScreenBinding
 
-class SplashScreen : AppCompatActivity() {
-    var splash: ImageView? = null
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
-        splash = findViewById(R.id.splash)
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : BaseActivity<ActivitySplashScreenBinding>() {
+
+    companion object {
+        private const val splashscreenTiomeout = 4000
+    }
+
+    override fun initControl() {
         Handler().postDelayed({
             val i = Intent(applicationContext, MainActivity::class.java)
             startActivity(i)
             finish()
         }, splashscreenTiomeout.toLong())
+
+        val animator = ValueAnimator.ofFloat(0f, 1f)
+        animator
+            .addUpdateListener { animation: ValueAnimator ->
+                binding.animationView
+                    .setProgress(
+                        animation
+                            .animatedValue as Float
+                    )
+            }
+        animator.start()
     }
 
-    companion object {
-        private const val splashscreenTiomeout = 2000
-    }
+    override fun getViewBinding(): ActivitySplashScreenBinding =
+        ActivitySplashScreenBinding.inflate(layoutInflater)
 }
